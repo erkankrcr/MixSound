53c1d7d6c7986a4b5ed3b6f9259588327912b151
Erkan Karacar <erkankrcr@outlook.com.tr>
2020-02-13 17:27:03 +0300

    First Commit

--- app/src/main/java/live/codemy/mixsound/MainActivity.kt
+++ app/src/main/java/live/codemy/mixsound/MainActivity.kt
@@ -1,14 +1,40 @@
 package live.codemy.mixsound
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import live.codemy.mixsoundlib.MixSound
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val record = findViewById<ImageView>(R.id.Record)
+       val sound1 = findViewById<ImageView>(R.id.Sound1)
+       val sound2 = findViewById<ImageView>(R.id.Sound2)
+       val sound3 = findViewById<ImageView>(R.id.Sound3)
+       val sound4 = findViewById<ImageView>(R.id.Sound4)
+       val layoutA = findViewById<LinearLayout>(R.id.Sound12)
+       val layoutB = findViewById<LinearLayout>(R.id.Sound34)
+
+        var mixSound = MixSound.getInstance(this)
+
+        layoutA.visibility = View.VISIBLE
+        layoutB.visibility = View.VISIBLE
+
+        record.setOnClickListener {
+            mixSound.recordSound()
+        }
+
+    }
+
+    override fun startActivityForResult(intent: Intent?, requestCode: Int) {
+        super.startActivityForResult(intent, requestCode)
+
     }
 }
