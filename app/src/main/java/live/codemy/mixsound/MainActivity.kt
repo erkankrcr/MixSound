9befb8fc77b60cab1021bbe3bc49e56eb971a239
Erkan Karacar <erkankrcr@outlook.com.tr>
2020-02-13 20:13:35 +0300

    Değişiklikler

--- app/src/main/java/live/codemy/mixsound/MainActivity.kt
+++ app/src/main/java/live/codemy/mixsound/MainActivity.kt
@@ -14,22 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       val record = findViewById<ImageView>(R.id.Record)
-       val sound1 = findViewById<ImageView>(R.id.Sound1)
-       val sound2 = findViewById<ImageView>(R.id.Sound2)
-       val sound3 = findViewById<ImageView>(R.id.Sound3)
-       val sound4 = findViewById<ImageView>(R.id.Sound4)
-       val layoutA = findViewById<LinearLayout>(R.id.Sound12)
-       val layoutB = findViewById<LinearLayout>(R.id.Sound34)
 
         var mixSound = MixSound.getInstance(this)
-
-        layoutA.visibility = View.VISIBLE
-        layoutB.visibility = View.VISIBLE
-
-        record.setOnClickListener {
-            mixSound.recordSound()
-        }
 
     }
 
